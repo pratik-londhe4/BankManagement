853fb0a35fe37cc0c36d26ce51afd25a364b860b
blushstudio240 <theblushstudio24@gmail.com>
2022-03-15 18:41:36 +0530

    added table admin

---
+++ admin-table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[admin] (
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [email]    VARCHAR (50) NULL,
+    [password] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
